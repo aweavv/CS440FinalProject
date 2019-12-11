00f8811d86421d8a3466a9c4f37c6d6975ee99ca
Austen <drkknight2112@gmail.com>
2019-12-10 22:07:03 -0700

    basic structure

---
+++ main.py
@@ -0,0 +1,7 @@
+#main.py
+#used to run the rest of the program as well as display results of program
+from textProcessing.py import textProcessing
+
+if __name__ == "__main__":
+
+    textProcessing.importDocs()
---
+++ textProcessing.py
@@ -0,0 +1,11 @@
+#textProcessing.py
+#Contains function to import training & test documents, as well as function for text parsing.
+
+class textProcessing:
+
+    def importDocs():
+        pass
+
+
+    def prepDocs():
+        pass
